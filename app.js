70bb5635f08a15548ea43192237269b2a9d782dd
Wajih Tagourty <wajih.tagourty@gmail.com>
2020-04-08 12:29:02 +0300

    updated to a new app structure

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+//config
+app
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs");
+//routes
+app.use("/", require("./routes/index"));
+//app.use("/test", (req, res) => res.send({ test: "data" }));
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error("----- An error happened -----");
+  console.error(err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(err.status || 500);
+    // A limited amount of information sent in production
+    if (process.env.NODE_ENV === "production") res.json(err);
+    else
+      res.json(
+        JSON.parse(JSON.stringify(err, Object.getOwnPropertyNames(err)))
+      );
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+//export default app;
